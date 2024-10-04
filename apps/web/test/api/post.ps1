b35989216978835cacdf5d77df764305dd88e546
Jagadish Sau <jsau5337@gmail.com>
2024-10-04 23:57:40 +0530

    apps/web: change code for api testing

--- apps/web/test/api/post.ps1
+++ apps/web/test/api/post.ps1
@@ -5,10 +5,14 @@ param(
 $url = "http://localhost:3000/api/iot"
 
 if ($fixed) {
-    $jsonContent = Get-Content -Path "$PSScriptRoot/01-fixed-data.json" -Raw
+    $jsonContent = Get-Content -Path "$PSScriptRoot/fixed-data.json" -Raw
 } else {
-    node "$PSScriptRoot/03-generate-random-data.js"
-    $jsonContent = Get-Content -Path "$PSScriptRoot/02-random-data.json" -Raw
+    $randomDataJsonPath = "$PSScriptRoot/random-data.json"
+    if (-not (Test-Path -Path $randomDataJsonPath)) {
+        New-Item -Path $randomDataJsonPath -ItemType File
+    }
+    node "$PSScriptRoot/generate-random-data.js"
+    $jsonContent = Get-Content -Path $randomDataJsonPath -Raw
 }
 
 Invoke-RestMethod -Uri $url -Method Post -Body $jsonContent -ContentType "application/json"
